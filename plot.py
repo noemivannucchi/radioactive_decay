105b92be5e27e602b1fc9868cb9d109b698b138b
Noemi Vannucchi <79851600+noemivannucchi@users.noreply.github.com>
2022-08-28 18:37:49 +0200

    imported files and added comments

--- plot.py
+++ plot.py
@@ -1,7 +1,13 @@
 import matplotlib.pyplot as plt
+from readFile import readFile
+from ode import N
 
 # plot results
 plt.plot(t,N,'r-',linewidth=2)
+
+# set x and y labels
 plt.xlabel('time t')
 plt.ylabel('N(t)')
+
+# put the grid
 plt.grid()
