ddb3a91c8797980eb38eedd8faef654817bc3c21
Noemi Vannucchi <79851600+noemivannucchi@users.noreply.github.com>
2022-08-28 18:34:35 +0200

    added docstring and comment

--- setn0.py
+++ setn0.py
@@ -1,5 +1,7 @@
-
-# set initial condition
 def setN0 ():
+    """ set the initial condition
+    of the differential states 
+    """
+    # ask the user to insert the initial condition to solve ODE
     N0 = input('Insert the initial number of radioactive nuclei: ')
     return N0
