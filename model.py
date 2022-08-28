972e956238c4a7f1cfd13ed79955c1e34779c979
Noemi Vannucchi <79851600+noemivannucchi@users.noreply.github.com>
2022-08-28 18:31:58 +0200

    added docstring and comments

--- model.py
+++ model.py
@@ -1,4 +1,8 @@
-# function that returns dN/dt
 def model(N,t,k):
+    """returns derivative values at requested N and t values 
+    as dNdt = model(N,t,k). The model represents an exponential decay
+    with a decay constant k.
+    """
+    #define the ordinary differential equation (ODE) of the model
     dNdt = -k * N
     return dNdt
