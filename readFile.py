463708032b71995ff3aef8644c9d63aa2e6c0e41
Noemi Vannucchi <79851600+noemivannucchi@users.noreply.github.com>
2022-08-28 18:33:33 +0200

    changed the way to put the file into an array

--- readFile.py
+++ readFile.py
@@ -1,6 +1,12 @@
-# time points
-def readFile(fileName):
-        fileObj = open(fileName, "r") #opens the file in read mode
-        t = fileObj.read().splitlines() #puts the file into an array
-        fileObj.close()
-        return t
+import numpy as np
+
+def readFile():
+    """ Reads from a text file the time values to be used in the model 
+    and puts them into an array t
+    """
+    # ask the user to insert the name of a text file with time values
+    filename = input('Insert the filename with the times: ')
+    
+    # put the time values from the text file into an array t
+    t = np.loadtxt(filename, unpack = 'True')
+    return t
