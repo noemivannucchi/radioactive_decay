2f9285328e5aac42d9f41ac1ea9f52cafceefdf8
Noemi Vannucchi <79851600+noemivannucchi@users.noreply.github.com>
2022-08-31 22:41:17 +0200

    changed function

--- setnuclei.py
+++ setnuclei.py
@@ -1,14 +1,19 @@
-def setnuclei ():
-    """ set the type of radioactive nuclei and the relative decay constant k
+def verifynuclei (nuclei):
+    """ Verify the type of radioactive nuclei and set the relative decay constant k
     """
-    #ask the user to choose the type of nuclei from a list
-    nuclei = input('Select the type of radioactive nuclei among "Uranium238", "Plutonium239" or "Radium226": ')
-    
     #set the decay constant k corresponding to the selected nucleus
-    if nuclei == 'Uranium238':
-        k = 5e-18
-    if nuclei == 'Plutonium239':
-        k = 9e-13
-    if nuclei == 'Radium226':
-        k = 1e-11
-    return k
+    if isinstance(nuclei,str):
+       if nuclei == 'Uranium238':
+           k = 5e-18
+           return k
+       if nuclei == 'Plutonium239':
+           k = 9e-13
+           return k
+       if nuclei == 'Radium226':
+           k = 1e-11
+           return k
+       else:
+            raise ValueError('Select the type of radioactive nuclei among "Uranium238", "Plutonium239" or "Radium226"')
+    else:
+        raise TypeError('The type of radioactive nuclei must be a string')
+        
