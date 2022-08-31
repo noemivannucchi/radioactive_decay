350eed2c9c341c1c8945316e6f7a6793a841211b
Noemi Vannucchi <79851600+noemivannucchi@users.noreply.github.com>
2022-08-31 22:38:30 +0200

    changed function

--- readFile.py
+++ readFile.py
@@ -1,12 +1,13 @@
-import numpy as np
+import numpy as np 
 
-def readFile():
-    """ Reads from a text file the time values to be used in the model 
-    and puts them into an array t
+def verifyFile(t):
+    """ Verify the array t with the time values (in s) to be used in the model 
     """
-    # ask the user to insert the name of a text file with time values
-    filename = input('Insert the filename with the times: ')
-    
-    # put the time values from the text file into an array t
-    t = np.loadtxt(filename, unpack = 'True')
-    return t
+    for elem in t:
+       if isinstance(elem, float):
+          if elem >= 0:
+             return np.sort(t)
+          else:
+             raise ValueError('The time values must be positive')
+       else:
+          raise TypeError('The time values must be floats')
