95e8e1e42f4320fe6014c8f4f813f22f35ac48f6
Noemi Vannucchi <79851600+noemivannucchi@users.noreply.github.com>
2022-09-01 11:42:15 +0200

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -1,4 +1,62 @@
 from model import model
 from hypothesis import given, strategies as st
 import pytest
+import numpy as np
 
+def test_k_float(): #positive test
+    k = 1.5
+    N = 5
+    t = 2
+    assert model(N,t,k) == -7.5
+
+def test_k_float_2(): #positive test
+    k = 1e3
+    N = 5
+    t = 2
+    assert model(N,t,k) == -5e3  
+    
+def test_N_positive(): #positive test
+    k = 1.5
+    N = 5
+    t = 2
+    assert model(N,t,k) == -7.5    
+
+def test_k_positive(): #positive test
+    k = 1.5
+    N = 5
+    t = 2
+    assert model(N,t,k) == -7.5    
+
+def test_TypeError_k():
+    k = np.linspace(0,1)
+    N = 5
+    t = 2   
+    with pytest.raises(TypeError):
+         model(N,t,k)
+
+def test_ValueError_N():
+    k = 1.5
+    N = -5
+    t = 2   
+    with pytest.raises(ValueError):
+        model(N,t,k)
+
+def test_ValueError_k():
+    k = -1.5
+    N = 5
+    t = 2   
+    with pytest.raises(ValueError):
+        model(N,t,k)
+
+def test_zero_ValueError_k():
+    k = 0.0
+    N = 5
+    t = 2 
+    with pytest.raises(ValueError):
+        model(N,t,k)
+        
+def test_result_float(): 
+    k = 1.5
+    N = 5
+    t = 2
+    assert isinstance(model(N,t,k) , float)       
