a0a4af87730ef478a126b5c2d694f40f5446adf4
Noemi Vannucchi <79851600+noemivannucchi@users.noreply.github.com>
2022-09-05 23:18:26 +0200

    added property tests

--- test_readFile.py
+++ test_readFile.py
@@ -3,7 +3,34 @@ from readFile import verifyFile
 from hypothesis import given, strategies as st
 import pytest
 
-def test_elements_float(): #positive test
+#===========================
+#PROPERTY TESTING 
+#===========================
+@given(a = st.lists(elements = st.floats(min_value=0)))
+def test_good_values_h(a):
+    if a != []:
+       b = np.array(a)
+       c = np.sort(b)
+       assert verifyFile(c).all() == c.all()
+
+@given(a = st.lists(elements = st.floats(max_value=0, exclude_max = True)))
+def test_ValueError_h(a):
+    if a != []:
+       with pytest.raises(ValueError):
+          verifyFile(a)
+
+@given(a = st.lists(elements = st.text()))
+def test_TypeError_h(a):
+    if a != []:
+       with pytest.raises(TypeError):
+          verifyFile(a)
+
+#===========================
+#UNIT TESTING 
+#all the possible positive and negative combinations
+#===========================
+
+def test_elements_float():
     a = np.array([1.5,1e3,1.5e5,1e7,1.5e11]) 
     assert verifyFile(a).all() == a.all()
 
