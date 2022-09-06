62f0aa45b7210c0057f3a09270d65333c12a944d
Noemi Vannucchi <79851600+noemivannucchi@users.noreply.github.com>
2022-09-06 23:22:46 +0200

    modified comment

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from readFile import verifyFile
 from ode import N, t
 
-# plot results
+# plot the number N of remaining nuclei after a time t vs time t (in s)
 plt.plot(verifyFile(t),N,'r-',linewidth=2)
 
 # set x and y labels
