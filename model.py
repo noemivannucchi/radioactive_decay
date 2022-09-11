6d5d8d3aa078ef1e4df25f0a2a469c3226f9dabd
Noemi Vannucchi <79851600+noemivannucchi@users.noreply.github.com>
2022-09-11 13:00:06 +0200

    updated docstring

--- model.py
+++ model.py
@@ -1,8 +1,15 @@
 def model(N,t,k):    
-    """returns derivative values at requested N and t values 
-    as dNdt = model(N,t,k). The model represents an exponential decay
-    with a decay constant k (in s^-1). 
-    N is the number of remaining nuclei after a time t (in s).
+    """
+    The input N is the number of remaining nuclei after a time t (in s), 
+    while the input k is the decay constant (in s^-1)
+    
+    This model represents an exponential decay with a decay constant k (in s^-1)
+    
+    This function checks that k is a float value greater than 0 and that N is positive
+    Then it returns derivative values at requested N and t values as dNdt = model(N,t,k)
+   
+    If N is not positive or k is not greater than 0, it raises a ValueError,
+    while if k is not a float value, it raises  TypeError
     """
     #verify that k is a float value
     if isinstance(k, float):
