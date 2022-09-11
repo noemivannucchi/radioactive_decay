2560d57cb32cefcab0b8bdcfe6af5cd271e118c2
Noemi Vannucchi <79851600+noemivannucchi@users.noreply.github.com>
2022-09-11 12:05:35 +0200

    Inserted N, t from external files
    t is now taken from a configuration file
    N is loaded from the text file 'results.txt'

--- plot.py
+++ plot.py
@@ -1,9 +1,19 @@
 import matplotlib.pyplot as plt
-from readFile import verifyFile
-from ode import N, t
+from configparser import ConfigParser
+import numpy as np
+
+#load the array with 'odeint' results from the text file 'results.txt'
+N = np.loadtxt('results.txt', dtype=float)
+
+#read the configuration file 'config.ini' and get the value corresponding to 'filename'
+parser = ConfigParser()
+parser.read('config.ini')
+filename = parser.get('config', 'filename')
+# put the time values from the text file 'filename' into an array t
+t = np.loadtxt(filename, unpack = 'True')
 
 # plot the number N of remaining nuclei after a time t vs time t (in s)
-plt.plot(verifyFile(t),N,'r-',linewidth=2)
+plt.plot(t,N,'r-',linewidth=2)
 
 # set x and y labels
 plt.xlabel('time t')
