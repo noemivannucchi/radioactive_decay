2ab549e471d983e146d5c400d92ddb7108b1e819
Noemi Vannucchi <79851600+noemivannucchi@users.noreply.github.com>
2022-09-14 12:18:19 +0200

    updated docstring

--- fit_funct.py
+++ fit_funct.py
@@ -2,16 +2,28 @@ import numpy as np
 
 def fitfunct(x, N0, k, b):
     """
+    Parameters
+    ----------
+    x : float
+        it is the variable of the function.
+    N0 : float
+        it is a float positive constant.
+    k : float
+        it is a float positive constant.
+    b : float
+        it is a float constant.
+    
+    Description
+    ----------
     This is the definition of the function for the fit.
     It represents a decreasing exponential functon.
     
-    The input values are: 
-        - x which is the variable of the function
-        - N0, k which are float positive constants
-        - b which is a float constant
-    
+    Returns
+    -------
     This function returns a decreasing exponential function
     which depends on the variable x
-    using the input parameters
+    using the input parameters.
+
     """
+    #return a decreasing exponential function in the variable x with N0, k and b constants
     return N0 * np.exp(-k * x) + b
