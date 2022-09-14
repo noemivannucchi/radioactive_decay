3d5e0fe31f0150cda77fa0a9b9b11b4d38272be6
Noemi Vannucchi <79851600+noemivannucchi@users.noreply.github.com>
2022-09-14 12:20:22 +0200

    added file for function for inter extra polation

---
+++ inter_extra_function.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+def interextra(x, N0, k, b, t):
+    """
+
+    Parameters
+    ----------
+    x : float
+        it represents the time value (in s) at which interpolation/extrapolation is performed
+    N0 : float
+        parameter extracted from the fit. It represents the initial number of decaying nuclei.
+    k : float
+        parameter extracted from the fit. It represents the decay constant.
+    b : float
+        parameter extracted from the fit. It represents a shift in the exponential fitting function.
+    t : numpy.array
+        array with the time values for ODE integration
+
+    Description
+    ----------
+    This function calculate the number of remaining nuclei N at the selected time x.
+    It is based on an interpolation/extrapolation from the fitting function N0 * np.exp(-k * x) + b
+    using the parameters extracted from the fit N0, k, b.
+  
+    Returns
+    -------
+    This function checks if the input time 'x' is whitin the minimum and the maximum time values of the array 't',
+    thus distinguishing the case of interpolation and extrapolation, 
+    and print the number of remaining nuclei N at the selected time x.
+    
+    """
+    # define the decreasing exponential function 'N', which is the same as the fitting function
+    N = N0 * np.exp(-k * x) + b
+    # convert the input array into a list 'l'
+    l = t.tolist()
+    # check if the input 'x' is whitin the minimum and the maximum time values of the array 't'
+    if min(l) < x < max(l):
+        # if 'x' is within the minimum and the maximum values of the array 't',
+        # it is the interpolation case and it is printed the resulting 
+        # number of remaining nuclei N at the time x
+        print (f"Interpolation: if t = {x}, N = {N}" )
+    else:
+        # otherwise, it is the extrapolation case and it is printed the resulting
+        # number of remaining nuclei N at a future time x
+        print (f"Extrapolation: if t = {x}, N = {N}" )
